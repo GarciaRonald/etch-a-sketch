b4a5b1e3f3a164a9fd3a44fce5e814c922a374f8
Ronald Garcia <ronaldgm@gmail.com>
2025-02-22 20:24:30 -0500

    Add button to toggle randomization on or off

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,6 +3,15 @@
 const sizeButton = document.querySelector("#grid-size");
 const gridContainer = document.querySelector("#grid-container");
 let gridSize = 16;
+const randomButton = document.querySelector("#random-colors");
+const onOffText = document.querySelector("#random");
+let randomColor = false;
+
+// Toggle random colors
+randomButton.addEventListener("click", () => {
+    randomColor = !randomColor;
+    onOffText.textContent = randomColor ? "ON" : "OFF";
+});
 
 // Create grid
 const createGrid = gridSize => {
@@ -15,7 +24,10 @@ const createGrid = gridSize => {
         gridContainer.appendChild(gridCell);
 
         gridCell.addEventListener("mouseover", () => {
-            gridCell.style.backgroundColor = "blue";
+            if (!randomColor) {
+                gridCell.style.backgroundColor = "rgb(0, 0, 255)";
+                gridCell.style.opacity = "1";
+            }
         });
     }
 };
