05e390f7b8511abd48126febf21fc05afe25270e
Ronald Garcia <ronaldgm@gmail.com>
2025-02-22 19:33:18 -0500

    Add function to allow user to enter dimensions for new grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,33 @@
 "use strict"
 
+const sizeButton = document.querySelector("#grid-size");
 const gridContainer = document.querySelector("#grid-container");
-const gridSize = 16;
+let gridSize = 16;
 
-// Create 16x16 grid
-for (let gridEle = 1; gridEle <= gridSize * gridSize; gridEle++) {
-    const gridCell = document.createElement("div");
-    gridCell.style.width = `${100 / gridSize}%`;
-    gridCell.style.height = `${100 / gridSize}%`;
+// Create grid
+const createGrid = gridSize => {
+    gridContainer.innerHTML = "";
+    for (let gridEle = 1; gridEle <= gridSize * gridSize; gridEle++) {
+        const gridCell = document.createElement("div");
+        gridCell.style.width = `${100 / gridSize}%`;
+        gridCell.style.height = `${100 / gridSize}%`;
 
-    gridContainer.appendChild(gridCell);
+        gridContainer.appendChild(gridCell);
 
-    // Event listener to highlight cell on hover
-    gridCell.addEventListener("mouseover", () => {
-        gridCell.style.backgroundColor = "blue";
-    });
-}
+        gridCell.addEventListener("mouseover", () => {
+            gridCell.style.backgroundColor = "blue";
+        });
+    }
+};
+
+createGrid(gridSize);
+
+// Change grid size based on user input
+sizeButton.addEventListener("click", () => {
+    gridSize = parseInt(prompt("Please enter a new grid size. Min: 1; Max: 100"));
+    if (gridSize < 1 || gridSize > 100) {
+        alert("Please enter a number between 1 and 100");
+        return;
+    }
+    createGrid(gridSize);
+});
